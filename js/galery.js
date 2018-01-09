85659e2b80dda1e3bcd61605f3ff388d8e15994b
Brandon-Struik <33702354+Brandon-Struik@users.noreply.github.com>
2018-01-09 14:25:55 +0100

    Fix voor images paths

--- js/galery.js
+++ js/galery.js
@@ -5,14 +5,16 @@ $( document ).ready(function() {
 		success: function(response) {
 			data = response.split("\n");
 			data.forEach(function(image) {
-				var newElement = element.clone();
-				var title = image.split('-')[0];
-				var description = image.split('-')[1].slice(0,-4);
-				newElement.find('.materialboxed')[0].setAttribute('src', 'images/' +image);
-				$(newElement.find('.card-title')[0]).html(title);
-				$(newElement.find('.card-description')[0]).html(description);
-				newElement.removeClass('hide');
-				$('#imageGalery').append(newElement);
+				if(image){
+					var newElement = element.clone();
+					var title = image.split('-')[0];
+					var description = image.split('-')[1].slice(0,-4);
+					newElement.find('.materialboxed')[0].setAttribute('src', 'galery/' +image);
+					$(newElement.find('.card-title')[0]).html(title);
+					$(newElement.find('.card-description')[0]).html(description);
+					newElement.removeClass('hide');
+					$('#imageGalery').append(newElement);
+				}
 			});
 		},
 		dataType: "json",

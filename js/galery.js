81f9c606c6cda0a892e4701b4a92f5201a229f71
Brandon-Struik <33702354+Brandon-Struik@users.noreply.github.com>
2018-03-07 20:21:42 +0100

    nieuwe plaatjes

--- js/galery.js
+++ js/galery.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
-    var element = $(`<div class="col s12 m4 hide">
-        <div class="card brown lighten-2">
+    var element = $(`
+    	<div class="card brown lighten-2">
           <div class="card-image">
             <img class="materialboxed" src="#">
           </div>
@@ -8,8 +8,7 @@ $( document ).ready(function() {
             <span class="card-title"></span>
             <p class="card-description"></p>
           </div>
-        </div>
-      </div>`);
+        </div>`);
 	$.ajax({
 		url:'js/data.txt',
 		success: function(response) {
@@ -23,14 +22,21 @@ $( document ).ready(function() {
 					if(image.split('-')[1]) {
 						var description = image.split('-')[1].slice(0,-4);
 						$(newElement.find('.card-description')[0]).html(description);
-						newElement.find('.materialboxed')[0].setAttribute('data-caption', description);
+						//newElement.find('.materialboxed')[0].setAttribute('data-caption', description);
 					} else {
 						title = title.slice(0,-5);
 					}
 					newElement.find('.materialboxed')[0].setAttribute('data-lightbox', title.replace("/ /g", "-"));
 					$(newElement.find('.card-title')[0]).html(title);
 					newElement.removeClass('hide');
-					$('#imageGalery').append(newElement);
+					var columns = ['#column1', '#column2', '#column3'];
+					columns.sort(function(column1, column2) {
+						var height1 = (column1? $(column1).height() : 0);
+						var height2 = (column2? $(column2).height() : 0);
+						return  height1 - height2;
+					});
+					$(columns[0]).append(newElement.clone());
+					$('#mobile-column').append(newElement);
 				}
 			});
 			$('.materialboxed').materialbox();
